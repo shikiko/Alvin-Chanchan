169b6b2e8c13054dc64d8bb67494729e9bc16cdc
Shikiko <syncviper@hotmail.com>
2019-03-24 01:20:03 +0800

    Update register.php
    Added checks for empty fields and only will attempt insert if all fields are not empty.

--- register.php
+++ register.php
@@ -1,61 +1,68 @@
 <?php 
-   $currentPage = 'register';
-   require_once("config.php");
-   
-   // If user posts a request.
-   // TODO : Validate input!!!
-   // TODO: 
-   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   echo '<script>console.log("[DEBUG]POSTING")</script>';
-   $name = trim_input($_POST["name"]);
-   echo '<script>console.log("[DEBUG]Name:';
-   echo $name;
-   echo '")</script>';
-   
-   $email = trim_input($_POST["email"]);
-   echo '<script>console.log("[DEBUG]Email:';
-   echo $email;
-   echo '")</script>';
-   
-   $password = trim_input($_POST["password"]);
-   echo '<script>console.log("[DEBUG]Password:';
-   echo $password;
-   echo '")</script>';
-   
-   
-   // Create connection
-   $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-   // Check connection
-   if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+$currentPage = 'register';
+require_once("config.php");
+
+// If user posts a request.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $check = True;
+  echo '<script>console.log("[DEBUG]POSTING")</script>';
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+    $check = False;
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "email is required";
+    $check = False;
+  } else {
+    $name = test_input($_POST["email"]);
+  }
+
+  if (empty($_POST["password"])) {
+    $passwordErr = "password is required";
+    $check = False;
+  } else {
+    $name = test_input($_POST["password"]);
+  }
+
+
+  if($check == True){
+    // Create connection
+    $conn = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+        // Check connection
+    if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
    }
-   
+
    $sql = "INSERT INTO User (username, email, password)
    VALUES ('$email', '$name', '$password')";
-   
+
    if ($conn->query($sql) === TRUE) {
-      echo '<script>console.log("[DEBUG]New record created successfully")</script>';
+     echo '<script>console.log("[DEBUG]New record created successfully")</script>';
    } else {
-      $error = "Error: " . $sql . "<br>" . $conn->error;
-      echo '<script>console.log("[DEBUG]Password:';
-      echo $error;
-      echo '")</script>';  
-    }
+     $error = "Error: " . $sql . "<br>" . $conn->error;
+     echo '<script>console.log("[DEBUG]Password:';
+     echo $error;
+     echo '")</script>';  
    }
-   
-   function trim_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
-   }
-   
-   // Close connection
-   $conn->close();
-   ?>
-<!DOCTYPE html>
-<html>
-   <head>
+      // Close connection
+      $conn->close();
+  }//end of insert
+}// end of post request
+
+function trim_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+  <!DOCTYPE html>
+  <html>
+    <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="description" content="">
@@ -89,61 +96,61 @@
       <!-- Tweaks for older IEs--><!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-   </head>
-   <body>
+    </head>
+    <body>
       <div id="all">
       <?php include("header.inc.php"); ?>
       <div id="content">
-         <div class="container">
-            <div class="row">
-               <div class="col-lg-6">
-                  <div class="box">
-                     <h2 class="text-uppercase">New account</h2>
-                     <p class="lead">Not our registered customer yet?</p>
-                     <p>With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
-                     <hr>
-                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-                        <div class="form-group">
-                           <label for="name-login">Username</label>
-                           <input id="name-login" type="text" class="form-control"  name="name">
-                        </div>
-                        <div class="form-group">
-                           <label for="email-login">Email</label>
-                           <input id="email-login" type="text" class="form-control" name="email">
-                        </div>
-                        <div class="form-group">
-                           <label for="password-login">Password</label>
-                           <input id="password-login" type="password" class="form-control" name="password">
-                        </div>
-                        <div class="text-center">
-                           <button type="submit" class="btn btn-template-outlined"><i class="fa fa-user-md"></i> Register</button>
-                        </div>
-                     </form>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="box">
+                <h2 class="text-uppercase">New account</h2>
+                <p class="lead">Not our registered customer yet?</p>
+                <p>With registration with us new world of fashion, fantastic discounts and much more opens to you! The whole process will not take you more than a minute!</p>
+                <hr>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+                  <div class="form-group">
+                    <label for="name-login">Username</label>
+                    <input id="name-login" type="text" class="form-control"  name="name">
                   </div>
-               </div>
-               <div class="col-lg-6">
-                  <div class="box">
-                     <h2 class="text-uppercase">Login</h2>
-                     <p class="lead">Already our customer?</p>
-                     <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-                     <hr>
-                     <form action="customer-orders.html" method="get">
-                        <div class="form-group">
-                           <label for="email">Email</label>
-                           <input id="email" type="text" class="form-control">
-                        </div>
-                        <div class="form-group">
-                           <label for="password">Password</label>
-                           <input id="password" type="password" class="form-control">
-                        </div>
-                        <div class="text-center">
-                           <button type="submit" class="btn btn-template-outlined"><i class="fa fa-sign-in"></i> Log in</button>
-                        </div>
-                     </form>
+                  <div class="form-group">
+                    <label for="email-login">Email</label>
+                    <input id="email-login" type="text" class="form-control" name="email">
                   </div>
-               </div>
+                  <div class="form-group">
+                    <label for="password-login">Password</label>
+                    <input id="password-login" type="password" class="form-control" name="password">
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-template-outlined"><i class="fa fa-user-md"></i> Register</button>
+                  </div>
+                </form>
+              </div>
             </div>
-         </div>
+            <div class="col-lg-6">
+              <div class="box">
+                <h2 class="text-uppercase">Login</h2>
+                <p class="lead">Already our customer?</p>
+                <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+                <hr>
+                <form action="customer-orders.html" method="get">
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="text" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Password</label>
+                    <input id="password" type="password" class="form-control">
+                  </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-template-outlined"><i class="fa fa-sign-in"></i> Log in</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <?php include("footer.inc.php"); ?>
       <!-- Javascript files-->
@@ -159,6 +166,6 @@
       <script src="vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
       <script src="vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
       <script src="js/front.js"></script>
-   </body>
-</html>
+    </body>
+  </html>
 
