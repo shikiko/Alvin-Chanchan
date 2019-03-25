81f39a0bd08a700a41ef74f3fb837db6c35a3e87
Shikiko <syncviper@hotmail.com>
2019-03-25 10:09:48 +0800

    Updated config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define("DBHOST","34.85.67.233");
-define("DBNAME","project");
-define("DBUSER","root");
-define("DBPASS","fuckjiale");
+define("DBHOST","161.117.122.252");
+define("DBNAME","group4");
+define("DBUSER","group04");
+define("DBPASS","wDhfjyA9h32i8T2a");
 ?>
