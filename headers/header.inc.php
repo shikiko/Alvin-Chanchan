4bb004d49a50c6feb33453fc6cd8861a66f16f2b
Leong Shao Zhi <darkdragon956@gmail.com>
2019-04-01 16:45:11 +0800

    Update header.inc.php

--- headers/header.inc.php
+++ headers/header.inc.php
@@ -91,41 +91,7 @@ require_once("../php_scripts/functions.php");
                </li>
                <li class="nav-item dropdown <?php if($currentPage=='Search'){echo 'active';}?>"><a href="search.php">Search</b></a>
                </li>
-               <li class="nav-item dropdown menu-large">
-                  <a href="#" data-toggle="dropdown" class="dropdown-toggle">Portfolio <b class="caret"></b></a>
-                  <ul class="dropdown-menu megamenu">
-                     <li>
-                        <div class="row">
-                           <div class="col-lg-6"><img src="../img/template-homepage.png" alt="" class="img-fluid d-none d-lg-block"></div>
-                           <div class="col-lg-3 col-md-6">
-                              <h5>Portfolio</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="portfolio-2.html" class="nav-link">2 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-2.html" class="nav-link">2 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-3.html" class="nav-link">3 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-3.html" class="nav-link">3 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-4.html" class="nav-link">4 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-4.html" class="nav-link">4 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-detail.html" class="nav-link">Portfolio - detail</a></li>
-                                 <li class="nav-item"><a href="portfolio-detail-2.html" class="nav-link">Portfolio - detail 2</a></li>
-                              </ul>
-                           </div>
-                           <div class="col-lg-3 col-md-6">
-                              <h5>About</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="about.html" class="nav-link">About us</a></li>
-                                 <li class="nav-item"><a href="team.html" class="nav-link">Our team</a></li>
-                                 <li class="nav-item"><a href="team-member.html" class="nav-link">Team member</a></li>
-                                 <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-                              </ul>
-                              <h5>Marketing</h5>
-                              <ul class="list-unstyled">
-                                 <li class="nav-item"><a href="packages.html" class="nav-link">Packages</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </li>
-                  </ul>
+               <li class="nav-item dropdown <?php if($currentPage=='Contact Us'){echo 'active';}?>"><a href="contactus.php">Contact Us</b></a>
                </li>
                <!-- ========== FULL WIDTH MEGAMENU ==================-->
                <li class="nav-item dropdown menu-large">
@@ -220,25 +186,7 @@ require_once("../php_scripts/functions.php");
                   </ul>
                </li>
                <!-- ========== FULL WIDTH MEGAMENU END ==================-->
-               <!-- ========== Contact dropdown ==================-->
-               <li class="nav-item dropdown">
-                  <a href="javascript: void(0)" data-toggle="dropdown" class="dropdown-toggle">Contact <b class="caret"></b></a>
-                  <ul class="dropdown-menu">
-                     <li class="dropdown-item"><a href="contact.html" class="nav-link">Contact option 1</a></li>
-                     <li class="dropdown-item"><a href="contact2.html" class="nav-link">Contact option 2</a></li>
-                     <li class="dropdown-item"><a href="contact3.html" class="nav-link">Contact option 3</a></li>
-                  </ul>
-               </li>
-               <!-- ========== Contact dropdown end ==================-->
             </ul>
-         </div>
-         <div id="search" class="collapse clearfix">
-            <form role="search" class="navbar-form">
-               <div class="input-group">
-                  <input type="text" placeholder="Search" class="form-control"><span class="input-group-btn">
-                  <button type="submit" class="btn btn-template-main"><i class="fa fa-search"></i></button></span>
-               </div>
-            </form>
          </div>
       </div>
    </div>
