af680aa29e527e7401571e2ae868bfe7b9154f9e
Leong Shao Zhi <darkdragon956@gmail.com>
2019-04-01 17:03:56 +0800

    Contact us in progress

---
+++ pages/contactus.php
@@ -0,0 +1,131 @@
+<?php
+$currentPage = 'Contact Us';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="all,follow">
+        <!-- Bootstrap CSS-->
+        <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+        <!-- Font Awesome CSS-->
+        <link rel="stylesheet" href="../vendor/font-awesome/css/font-awesome.min.css">
+        <!-- Google fonts - Roboto-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+        <!-- Bootstrap Select-->
+        <link rel="stylesheet" href="../vendor/bootstrap-select/css/bootstrap-select.min.css">
+        <!-- owl carousel-->
+        <link rel="stylesheet" href="../vendor/owl.carousel/assets/owl.carousel.css">
+        <link rel="stylesheet" href="../vendor/owl.carousel/assets/owl.theme.default.css">
+        <!-- theme stylesheet-->
+        <link rel="stylesheet" href="../css/style.default.css" id="theme-stylesheet">
+        <!-- Custom stylesheet - for your changes-->
+        <link rel="stylesheet" href="../css/custom.css">
+        <!-- Favicon and apple touch icons-->
+        <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="../img/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="../img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="../img/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="../img/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="../img/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="../img/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="../img/apple-touch-icon-152x152.png">
+        <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <div id="all">
+      <?php
+      include ('../headers/header.inc.php');
+      ?>
+      <div id="content">
+        <div id="contact" class="container">
+          <section class="bar">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="heading">
+                  <h2>We are here to help you</h2>
+                </div>
+                <p class="lead">Need help with something? Drop us an email with your question!</p>
+                <p class="text-sm">Please feel free to contact us, we will get back to you as soon as we can.</p>
+              </div>
+            </div>
+          </section>
+          <section class="bar pt-0">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="heading text-center">
+                  <h2>Contact form</h2>
+                </div>
+              </div>
+              <div class="col-md-8 mx-auto">
+                <form method="POST" action="contactMail.php" name="contactusform">
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="firstname">First Name</label>
+                        <input id="firstname" type="text" class="form-control" name="CFname" required maxlength="50">
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="lastname">Last Name</label>
+                        <input id="lastname" type="text" class="form-control" name="CLname" required maxlength="50">
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="email" type="text" class="form-control" name="Cemail" required maxlength="80">
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label for="subject">Subject</label>
+                        <input id="subject" type="text" class="form-control" name="Csubject" required maxlength="50">
+                      </div>
+                    </div>
+                    <div class="col-sm-12">
+                      <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea id="message" class="form-control" name="Cmessage" required maxlength="1000"></textarea>
+                      </div>
+                    </div>
+                    <div class="col-sm-12 text-center">
+                      <button type="submit" class="btn btn-template-outlined" value="Csubmit"><i class="fa fa-envelope-o"></i> Send message</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+        </div>
+        <?php
+        include ('../headers/footer.inc.php');
+        ?>
+    </div>
+    <!-- Javascript files-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="../vendor/waypoints/lib/jquery.waypoints.min.js"> </script>
+    <script src="../vendor/jquery.counterup/jquery.counterup.min.js"> </script>
+    <script src="../vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="../vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+    <script src="../js/jquery.parallax-1.1.3.js"></script>
+    <script src="../vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="../vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
+    <script src="../js/gmaps.js"></script>
+    <script src="../js/gmaps.init.js"></script>
+    <script src="../js/front.js"></script>
+  </body>
+</html>
