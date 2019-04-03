1f5c12bd81df7cb907a06d3e23ef0b5ee575e69a
vizierofhope <limyongan8888@gmail.com>
2019-04-03 10:39:12 +0800

    Update insert_chat.php

--- pages/insert_chat.php
+++ pages/insert_chat.php
@@ -59,9 +59,8 @@ $connect = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
 function get_user_name($user_id)
 {
- $Username = $_SESSION['username'];
  $connect = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
- $query = "SELECT `Username` FROM `user` WHERE Username = '$Username'";
+ $query = "SELECT `Username` FROM `user` WHERE Username = '$user_id'";
  $statement = $connect->prepare($query);
  $statement->execute();
  $result = $statement->get_result()->fetch_all(MYSQLI_ASSOC);;
