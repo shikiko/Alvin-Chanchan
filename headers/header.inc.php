9d0fed885f343d3d901b9e6b99ecaf1a8a8f5eda
Shikiko <syncviper@hotmail.com>
2019-04-04 15:33:17 +0800

    fixed paging for pasar malam codes

--- headers/header.inc.php
+++ headers/header.inc.php
@@ -35,7 +35,7 @@ require_once("../php_scripts/functions.php");
                   echo '<div class="login"><a class="login-btn" href="../pages/profile.php?username='.($_SESSION["username"]);
                   echo'">
                   <i class="fa fa-user"></i><span class="d-none d-md-inline-block">My Profile</span></a>
-                  <a href="../pages/uploadListing" class="signup-btn"><i class="fa fa-balance-scale"></i>
+                  <a href="../pages/uploadListing.php" class="signup-btn"><i class="fa fa-balance-scale"></i>
                   <span class="d-none d-md-inline-block">Create Listing</span></a>
                   <a href="Inbox.php" class="signup-btn"><i class="fa fa-inbox"></i>
                   <span class="d-none d-md-inline-block">Messages</span></a>
@@ -101,100 +101,6 @@ require_once("../php_scripts/functions.php");
                   }
                };
                ?>
-
-               <!-- ========== FULL WIDTH MEGAMENU ==================-->
-               <!-- <li class="nav-item dropdown menu-large">
-                  <a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle">All Pages <b class="caret"></b></a>
-                  <ul class="dropdown-menu megamenu">
-                     <li>
-                        <div class="row">
-                           <div class="col-md-6 col-lg-3">
-                              <h5>Home</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="index.html" class="nav-link">Option 1: Default Page</a></li>
-                                 <li class="nav-item"><a href="index2.html" class="nav-link">Option 2: Application</a></li>
-                                 <li class="nav-item"><a href="index3.html" class="nav-link">Option 3: Startup</a></li>
-                                 <li class="nav-item"><a href="index4.html" class="nav-link">Option 4: Agency</a></li>
-                                 <li class="nav-item"><a href="index5.html" class="nav-link">Option 5: Portfolio</a></li>
-                              </ul>
-                              <h5>About</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="about.html" class="nav-link">About us</a></li>
-                                 <li class="nav-item"><a href="team.html" class="nav-link">Our team</a></li>
-                                 <li class="nav-item"><a href="team-member.html" class="nav-link">Team member</a></li>
-                                 <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-                              </ul>
-                              <h5>Marketing</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="packages.html" class="nav-link">Packages</a></li>
-                              </ul>
-                           </div>
-                           <div class="col-md-6 col-lg-3">
-                              <h5>Portfolio</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="portfolio-2.html" class="nav-link">2 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-2.html" class="nav-link">2 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-3.html" class="nav-link">3 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-3.html" class="nav-link">3 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-4.html" class="nav-link">4 columns</a></li>
-                                 <li class="nav-item"><a href="portfolio-no-space-4.html" class="nav-link">4 columns with negative space</a></li>
-                                 <li class="nav-item"><a href="portfolio-detail.html" class="nav-link">Portfolio - detail</a></li>
-                                 <li class="nav-item"><a href="portfolio-detail-2.html" class="nav-link">Portfolio - detail 2</a></li>
-                              </ul>
-                              <h5>User pages</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="customer-register.html" class="nav-link">Register / login</a></li>
-                                 <li class="nav-item"><a href="customer-orders.html" class="nav-link">Orders history</a></li>
-                                 <li class="nav-item"><a href="customer-order.html" class="nav-link">Order history detail</a></li>
-                                 <li class="nav-item"><a href="customer-wishlist.html" class="nav-link">Wishlist</a></li>
-                                 <li class="nav-item"><a href="customer-account.html" class="nav-link">Customer account / change password</a></li>
-                              </ul>
-                           </div>
-                           <div class="col-md-6 col-lg-3">
-                              <h5>Shop</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="shop-category.html" class="nav-link">Category - sidebar right</a></li>
-                                 <li class="nav-item"><a href="shop-category-left.html" class="nav-link">Category - sidebar left</a></li>
-                                 <li class="nav-item"><a href="shop-category-full.html" class="nav-link">Category - full width</a></li>
-                                 <li class="nav-item"><a href="shop-detail.html" class="nav-link">Product detail</a></li>
-                              </ul>
-                              <h5>Shop - order process</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="shop-basket.html" class="nav-link">Shopping cart</a></li>
-                                 <li class="nav-item"><a href="shop-checkout1.html" class="nav-link">Checkout - step 1</a></li>
-                                 <li class="nav-item"><a href="shop-checkout2.html" class="nav-link">Checkout - step 2</a></li>
-                                 <li class="nav-item"><a href="shop-checkout3.html" class="nav-link">Checkout - step 3</a></li>
-                                 <li class="nav-item"><a href="shop-checkout4.html" class="nav-link">Checkout - step 4</a></li>
-                              </ul>
-                           </div>
-                           <div class="col-md-6 col-lg-3">
-                              <h5>Contact</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                                 <li class="nav-item"><a href="contact2.html" class="nav-link">Contact - version 2</a></li>
-                                 <li class="nav-item"><a href="contact3.html" class="nav-link">Contact - version 3</a></li>
-                              </ul>
-                              <h5>Pages</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="text.html" class="nav-link">Text page</a></li>
-                                 <li class="nav-item"><a href="text-left.html" class="nav-link">Text page - left sidebar</a></li>
-                                 <li class="nav-item"><a href="text-full.html" class="nav-link">Text page - full width</a></li>
-                                 <li class="nav-item"><a href="faq.html" class="nav-link">FAQ</a></li>
-                                 <li class="nav-item"><a href="404.html" class="nav-link">404 page</a></li>
-                              </ul>
-                              <h5>Blog</h5>
-                              <ul class="list-unstyled mb-3">
-                                 <li class="nav-item"><a href="blog.html" class="nav-link">Blog listing big</a></li>
-                                 <li class="nav-item"><a href="blog-medium.html" class="nav-link">Blog listing medium</a></li>
-                                 <li class="nav-item"><a href="blog-small.html" class="nav-link">Blog listing small</a></li>
-                                 <li class="nav-item"><a href="blog-post.html" class="nav-link">Blog Post</a></li>
-                              </ul>
-                           </div>
-                        </div>
-                     </li>
-                  </ul>
-               </li> -->
-               <!-- ========== FULL WIDTH MEGAMENU END ==================-->
             </ul>
          </div>
       </div>
