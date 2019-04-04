a58542b2832ea514e3e4eae06fcc5077056c825b
Shikiko <syncviper@hotmail.com>
2019-04-04 21:46:06 +0800

    Removed Scripts and put it all at header.

--- pages/manageprofiles.php
+++ pages/manageprofiles.php
@@ -155,23 +155,5 @@
       }
     }
     </script>
-    <script src="//code.jquery.com/jquery.min.js"></script> <script type="text/javascript">
-    </script>
-    <script src="../thirdpartyvendors/jquery.tabledit.js"></script>
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/popper.js/umd/popper.min.js"> </script>
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../vendor/jquery.cookie/jquery.cookie.js"> </script>
-    <script src="../vendor/waypoints/lib/jquery.waypoints.min.js"> </script>
-    <script src="../vendor/jquery.counterup/jquery.counterup.min.js"> </script>
-    <script src="../vendor/owl.carousel/owl.carousel.min.js"></script>
-    <script src="../vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
-    <script src="../js/jquery.parallax-1.1.3.js"></script>
-    <script src="../vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
-    <script src="../vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
-    <script src="../js/gmaps.js"></script>
-    <script src="../js/gmaps.init.js"></script>
-    <script src="../js/front.js"></script>
   </body>
 </html>
