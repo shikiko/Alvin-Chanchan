1484767ed8a0445615318966864af9a8d8fd218e
Shikiko <syncviper@hotmail.com>
2019-04-04 15:47:04 +0800

    Removed Icons

--- headers/header.inc.php
+++ headers/header.inc.php
@@ -21,8 +21,8 @@ require_once("../php_scripts/functions.php");
          <div class="col-md-6">
             <div class="d-flex justify-content-md-end justify-content-between">
                <ul class="list-inline contact-info d-block d-md-none">
-                  <li class="list-inline-item"><a href="#"><i class="fa fa-phone"></i></a></li>
-                  <li class="list-inline-item"><a href="#"><i class="fa fa-envelope"></i></a></li>
+                  <li class="list-inline-item"><a href="#"></a></li>
+                  <li class="list-inline-item"><a href="#"></a></li>
                </ul>
                <?php
                echo '<script>console.log("[DEBUG]Session_Status():';
